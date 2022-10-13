2cf59000a3cd5ee14cd42f82c4381598be242d4d
Miguel Ortega <miguelorab@gmail.com>
2022-10-13 09:21:22 +0100

    Primera versión de Kata_3

---
+++ src/kata_3/HistogramDisplay.java
@@ -0,0 +1,47 @@
+package kata_3;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame {
+
+    public HistogramDisplay(String title) {
+        super(title);
+        setContentPane(createPanel());
+        pack();
+    }
+    
+    public void execute() {
+        setVisible(true);
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataSet()));
+        chartPanel.setPreferredSize(new Dimension(500, 400));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataset) {
+        JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart", "Dominios email", "Nº de emails", dataset, PlotOrientation.VERTICAL, false, false, rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataSet() {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        
+        dataset.addValue(2, "", "ulpgc.es");
+        dataset.addValue(7, "", "dis.ulpgc.es");
+        dataset.addValue(1, "", "gmail.com");
+        dataset.addValue(2, "", "hotmail.com");
+        dataset.addValue(15, "", "eii.ulpgc.es");
+        
+        return dataset;
+    }
+}
---
+++ src/kata_3/Kata_3.java
@@ -0,0 +1,10 @@
+package kata_3;
+
+public class Kata_3 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histogramDisplay = new HistogramDisplay("Histogram");
+        histogramDisplay.execute();
+    }
+    
+}
